444c11f5392ca7fb190b3b407eaa902767343ea1
Erik Zenker <erikzenker@hotmail.com>
2012-11-22 00:10:55 +0100

    * Minor changes

--- src/WebdavSyncManager.cc
+++
@@ -1,8 +0,0 @@
-#include "WebdavSyncManager.h"
-
-WebdavSyncManager::WebdavSyncManager(string destFolder, string syncType) : 
-  RemoteSyncManager(destFolder, syncType){
-  
-  //mDestFolder = destFolder;
-
-}
